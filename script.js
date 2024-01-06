8cd428360123642d760d5f740f64c78022e8a3d8
henriet <henriet@irif.fr>
2024-01-06 14:30:26 +0100

    add kopek img show

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let kopek = 0;
+
 function gagner () {
+    // Increment kopeck variable
     let a = document.getElementById("kopek");
     console.log(a)
     kopek += 1;
     a.innerHTML = "Actuellement " + kopek + " kopeks.";
+
+    // Add random kopeck img
+    let kopek_img = document.createElement("img");
+    kopek_img.src = "kopek.jpeg";
+    kopek_img.width = 50;
+    kopek_img.height = 50;
+    document.body.appendChild(kopek_img);
 };
